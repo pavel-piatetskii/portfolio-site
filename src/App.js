79a13ae1309b8781bbb35db6a930248c0b2ab433
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-04-25 15:08:31 -0700

    Continue to work on Greetings animation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { useEffect, useState } from 'react';
 
 function App() {
 
   const [showGreeting, setShowGreeting] = useState(true);
   const [greeting, setGreeting] = useState('');
+  //const words = ['Здравствуйте', 'Bonjour', 'Ciao', 'Hola', 'Hello'];
+
+  //const iterateGreeting = function() {
+  //  const words = ['Здравствуйте', 'Bonjour', 'Ciao', 'Hola', 'Hello'];
+  //  for (const [index, word] of words.entries()) {
+  //    console.log(index, word)
+  //    setTimeout(() => setGreeting(word), index * 500)
+  //  }
+  //};
 
   const iterateGreeting = function() {
-    const words = ['Здравствуйте', 'Bonjour', 'Ciao', 'Hola', 'Hello'];
-    for (const [index, word] of words.entries()) {
-      console.log(index, word)
-      setTimeout(() => setGreeting(word), index * 1000)
-    }
+  const words = ['Здравствуйте', 'Bonjour', 'Ciao', 'Hola', 'Hello'];
+
+  return setTimeout(() => words.map((word) => {
+      const x = Math.random() * 60 + 20;
+      const y = Math.random() * 60 + 20;
+      return <p class="greetings__container" style={{top: `${x}vh`, left: `${y}vh`}}>{word}!</p>
+      }), 500)
   };
 
-  useEffect(() => iterateGreeting(), []);
+  //useEffect(() => iterateGreeting(), []);
 
   //setTimeout(() => setGreeting(false), 1000);
 
   return (
     <div className="App">
       <header className="App-header">
-      {showGreeting && <p>{greeting}!</p>}
+      {iterateGreeting()}
       {!showGreeting && <p>homepage</p>}
       </header>
     </div>
