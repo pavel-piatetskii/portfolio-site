4d7b61d15ed00d796c3f040ae04cebfb301894db
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-04-25 18:17:40 -0700

    Set random appearance for greetings

--- src/App.js
+++ src/App.js
@@ -5,36 +5,49 @@ function App() {
 
   const [showGreeting, setShowGreeting] = useState(true);
   const [greeting, setGreeting] = useState('');
+  const [greetingArr, setGreetingArr] = useState([]);
   //const words = ['Здравствуйте', 'Bonjour', 'Ciao', 'Hola', 'Hello'];
 
-  //const iterateGreeting = function() {
-  //  const words = ['Здравствуйте', 'Bonjour', 'Ciao', 'Hola', 'Hello'];
-  //  for (const [index, word] of words.entries()) {
-  //    console.log(index, word)
-  //    setTimeout(() => setGreeting(word), index * 500)
-  //  }
-  //};
-
-  const iterateGreeting = function() {
-  const words = ['Здравствуйте', 'Bonjour', 'Ciao', 'Hola', 'Hello'];
-
-  return setTimeout(() => words.map((word) => {
-      const x = Math.random() * 60 + 20;
-      const y = Math.random() * 60 + 20;
-      return <p class="greetings__container" style={{top: `${x}vh`, left: `${y}vh`}}>{word}!</p>
-      }), 500)
+  const iterateGreeting = function () {
+    const words = ['Здравствуйте', 'Bonjour', 'Ciao', 'Hola', 'Hello'];
+    for (const [index, word] of words.entries()) {
+      console.log(index, word)
+      setTimeout(() => setGreeting(word), (index + 2) * 500)
+    }
   };
 
-  //useEffect(() => iterateGreeting(), []);
+  //const iterateGreeting = function() {
+  //const words = ['Здравствуйте', 'Bonjour', 'Ciao', 'Hola', 'Hello'];
+  //const x = Math.random() * 60 + 20;
+  //const y = Math.random() * 60 + 20;
+  //<p class="greetings__container" style={{top: `${x}vh`, left: `${y}vh`}}>{'word'}!</p> 
+
+  //return setTimeout(() => words.map((word) => {
+  //    const x = Math.random() * 60 + 20;
+  //    const y = Math.random() * 60 + 20;
+  //    return <p class="greetings__container" style={{top: `${x}vh`, left: `${y}vh`}}>{word}!</p>
+  //    }), 500)
+  //
+  //};
+
+  useEffect(() => iterateGreeting(), []);
+  useEffect(() => {
+    const x = Math.random() * 60 + 20;
+    const y = Math.random() * 60 + 20;
+    console.log(x, y);
+    setGreetingArr(prev => [...prev, <p key={greetingArr.length} className="greetings__container" style={{ top: `${x}vh`, left: `${y}vh` }}>{greeting}!</p>])
+  }, [greeting]);
 
   //setTimeout(() => setGreeting(false), 1000);
 
   return (
     <div className="App">
       <header className="App-header">
-      {iterateGreeting()}
-      {!showGreeting && <p>homepage</p>}
+        {greeting && greetingArr.map((gr) => gr)}
+        {!showGreeting && <p>homepage</p>}
       </header>
+      <div className="main">
+      </div>
     </div>
   );
 }
