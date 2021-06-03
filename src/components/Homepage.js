ab35148ffbaffc35028cfe08dc3a0586c25edb2a
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-06-03 09:52:14 -0700

    Apply appearance animation to every content component

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,7 @@
 import './Homepage.scss';
 
 export default function Homepage(props) {
-  return (<section class="content__homepage">
+  return (<section class="homepage">
       <p>This is homepage</p>
     </section>)
 }
