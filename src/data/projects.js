3c8cb7c28fb397f01748390169cd1374a1e0b71b
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-06-05 16:22:14 -0700

    Add images for two projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,16 +1,17 @@
 const projects = [
   {
-    name: 'sample1',
-    image: 'https://static.wikia.nocookie.net/civilization/images/8/89/Aztec_%28Civ5%29.png',
+    name: 'Chat module',
+    image: 'https://lh3.googleusercontent.com/69x6Xx_gZcSV8MPoxbQ9n9i4miuk-u-hEshaKMfNXjsDW_JvWUBipS_wZQAo782Ubgao1bpEVN-MC0iD-3wv=w1853-h949-rw',
     description: 'description1',
     link: 'link1',
   },
   {
     name: 'sample2',
-    image: 'https://static.wikia.nocookie.net/civilization/images/8/89/Aztec_%28Civ5%29.png',
+    image: 'https://raw.githubusercontent.com/pavel-piatetskii/react-sage/master/docs/recipe_page.png',
     description: 'description2',
     link: 'link2',
   },
 ];
 
 export default projects;
+
