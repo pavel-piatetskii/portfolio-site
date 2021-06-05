84e0315f387f3256d864876775ada9c5b672cabe
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-06-05 16:29:28 -0700

    Add descriptions and change styling

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,14 +2,14 @@ const projects = [
   {
     name: 'Chat module',
     image: 'https://lh3.googleusercontent.com/69x6Xx_gZcSV8MPoxbQ9n9i4miuk-u-hEshaKMfNXjsDW_JvWUBipS_wZQAo782Ubgao1bpEVN-MC0iD-3wv=w1853-h949-rw',
-    description: 'description1',
-    link: 'link1',
+    description: 'Live chat appllicaion with separate rooms',
+    link: 'https://github.com/pavel-piatetskii/chat-module',
   },
   {
-    name: 'sample2',
+    name: 'CocktailSage',
     image: 'https://raw.githubusercontent.com/pavel-piatetskii/react-sage/master/docs/recipe_page.png',
-    description: 'description2',
-    link: 'link2',
+    description: 'Cocktail Sage is a website where you can browse or search existing recipes, add their own twist to them or create a new recipe from scratch!',
+    link: 'https://github.com/pavel-piatetskii/react-sage',
   },
 ];
 
