eabe4efc3640f19b3d6de79a66e713a7d6523064
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-06-05 17:02:59 -0700

    Add more projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,11 @@
 const projects = [
   {
+    name: 'Portfolio website',
+    image: 'https://miro.medium.com/max/2048/1*mwXHpdt6CTQHxH78dwc6NA.jpeg',
+    description: 'My personal portfolio website',
+    link: 'https://github.com/pavel-piatetskii/portfolio-site',
+  },
+  {
     name: 'Chat module',
     image: 'https://lh3.googleusercontent.com/69x6Xx_gZcSV8MPoxbQ9n9i4miuk-u-hEshaKMfNXjsDW_JvWUBipS_wZQAo782Ubgao1bpEVN-MC0iD-3wv=w1853-h949-rw',
     description: 'Live chat appllicaion with separate rooms',
@@ -10,6 +16,30 @@ const projects = [
     image: 'https://raw.githubusercontent.com/pavel-piatetskii/react-sage/master/docs/recipe_page.png',
     description: 'Cocktail Sage is a website where you can browse or search existing recipes, add their own twist to them or create a new recipe from scratch!',
     link: 'https://github.com/pavel-piatetskii/react-sage',
+  },
+  {
+    name: 'Huawei Autoconfig',
+    image: 'https://raw.githubusercontent.com/pavel-piatetskii/huawei_autoconfig/master/docs/preview.png',
+    description: 'This project was created to compose configuration files for Huawei switches fast and error-free. It can make it from scratch or use an existing configuration from D-Link or Huawei switch to transfer some parameters like VLANS, port descriptions, etc. It has convenient Tk-based GUI',
+    link: 'https://github.com/pavel-piatetskii/huawei_autoconfig',
+  },
+  {
+    name: 'DevTips',
+    image: 'https://raw.githubusercontent.com/pavel-piatetskii/midterm-resourcewall/master/docs/home-page.png',
+    description: 'DevTips is a website where everybody can share code snippets or links to articles, blogs, videos',
+    link: 'https://github.com/pavel-piatetskii/midterm-resourcewall',
+  },
+  {
+    name: 'Interview Scheduler',
+    image: 'https://raw.githubusercontent.com/pavel-piatetskii/scheduler/master/docs/general-view.png',
+    description: 'Interview Scheduler is an application that helps to plan appointments. Depending on the day there are several interviewers who can speak to a student during an available time. The application allows to book appointment at one of five 1-hour timeslots each workday. Every existing appointment can be edited or cancelled',
+    link: 'https://github.com/pavel-piatetskii/scheduler',
+  },
+  {
+    name: 'Tweeter',
+    image: 'https://raw.githubusercontent.com/pavel-piatetskii/tweeter/master/docs/desktop-main.png',
+    description: 'Tweeter is a single-page application which aimed to help people exchange short messages',
+    link: 'https://github.com/pavel-piatetskii/tweeter',
   },
 ];
 
