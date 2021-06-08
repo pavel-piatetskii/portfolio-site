6d895fd3967929d723bdaa4d2322700003ee8d51
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-06-07 23:03:19 -0700

    Start implementing buttons for ImageViewer

--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -7,6 +7,7 @@ export default function ImageViewer(props) {
   //const heightShift = image.naturalHeight / 2;
   //const widthShift = image.naturalWidth / 2;
 
+
   const clickSomewhere = function(e) {
     return (e.target.className === "image-viewer") ? setShowImageViewer(false) : false;
   }
@@ -15,6 +16,10 @@ export default function ImageViewer(props) {
   return (
     <section class="image-viewer" onClick={(e) => clickSomewhere(e)}>
       <img className="image-viewer__image" src={image.src}/>
+      <div className="image-viewer__buttons">
+        <p className="image-viewer__buttons__new-tab">Open</p>
+        <p className="image-viewer__buttons__close">Close</p>
+      </div>
     </section>
   )
 }
