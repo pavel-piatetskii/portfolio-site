4358a2a1f86f817c6de2071ca72f6fd8bae0687a
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-06-09 19:49:50 -0700

    Add zoom ability for Viewer

--- src/components/ImageViewer/ImageViewer.js
+++ src/components/ImageViewer/ImageViewer.js
@@ -1,14 +1,18 @@
+import { useEffect } from 'react';
 import './ImageViewer.scss';
 
 export default function ImageViewer(props) {
 
   const { image, setShowImageViewer } = props;
-
+  let zoom = 1;
 
   // Click outside an image closes ImageViewer
 
   const clickSomewhere = function(e) {
-    return (e.target.className === "image-viewer") ? setShowImageViewer(false) : false;
+    if (e.target.className === "image-viewer") {
+      setShowImageViewer(false);
+    }
+    return false;
   };
 
 
@@ -18,10 +22,21 @@ export default function ImageViewer(props) {
   const openNewTab = function(url) {
     window.open(url, '_blank').focus();
   }
-  
+
+  const zoomOnScroll = function(e) {
+    e.preventDefault();
+    zoom += (e.deltaY > 0 ) ? -0.1 : 0.1;
+    document.getElementsByClassName('image-viewer__image')[0].style.zoom = zoom;
+  }
+
+  useEffect(() => {
+    window.addEventListener('wheel', zoomOnScroll, {passive: false});
+    return () => window.removeEventListener('wheel', zoomOnScroll)
+  }, [])
+
   return (
     <section class="image-viewer" onClick={(e) => clickSomewhere(e)}>
-      <img className="image-viewer__image" src={image} alt="project preview"/>
+      <img className="image-viewer__image" src={image} alt="project preview" />
       <div className="image-viewer__buttons">
         <img src="newtab.png" className="image-viewer__buttons__new-tab" alt="open in new tab"
              onClick={() => openNewTab(image)}></img>
