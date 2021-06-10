d9f27cceed0d6ee39d867242d1221675541d2f66
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-06-10 11:39:07 -0700

    Add link to every contact to call, mail, or open a page

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,42 +1,21 @@
 import './Contacts.scss';
+import contactsData from 'data/contacts'
 
 export default function Contacts(props) {
 
-  const contactsData = [
-    {
-      name: 'E-Mail',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png',
-      value: 'pavel.piatetskii@gmail.com',
-      link: '#',
-    },
-    {
-      name: 'Phone',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Phone_Shiny_Icon.svg/1024px-Phone_Shiny_Icon.svg.png',
-      value: '604-363-17-23',
-      link: '#',
-    },
-    {
-      name: 'GitHub',
-      image: 'github-logo.png',
-      value: 'https://github.com/pavel-piatetskii',
-      link: '#',
-    },
-    {
-      name: 'LinkedIn',
-      image: 'linkedin-logo.png',
-      value: 'https://www.linkedin.com/in/pavel-piatetskii-1887241a0/',
-      link: '',
-    },
-  ];
-
   return (<section class="contacts">
     <h2 className="contacts__header">Contacts</h2>
-    {contactsData.map(contact => (
-      <article className="contacts__container">
-        <img className="contacts__container__image" src={contact.image} />
-        <span className="contacts__container__name">{contact.name}:</span>
-        <span className="contacts__container__value">{contact.value}</span>
-      </article>
-    ))}
+    {contactsData.map(contact => {
+      const { image, name, value, link, newtab } = contact;
+      return (
+        <article className="contacts__container">
+          <img className="contacts__container__image" src={image} />
+          <span className="contacts__container__name">{name}:</span>
+          <a className="contacts__container__link" rel={newtab && "noreferrer noopener"} target={newtab && "_blank"} href={link}>
+            <span className="contacts__container__value">{value}</span>
+          </a>
+        </article>
+      )}
+    )}
   </section>)
 }
---
+++ src/data/contacts.js
@@ -0,0 +1,32 @@
+const contactsData = [
+  {
+    name: 'E-Mail',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png',
+    value: 'pavel.piatetskii@gmail.com',
+    link: 'mailto:pavel.piatetskii@gmail.com',
+    newtab: false,
+  },
+  {
+    name: 'Phone',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Phone_Shiny_Icon.svg/1024px-Phone_Shiny_Icon.svg.png',
+    value: '604-363-17-24',
+    link: 'tel:+16043631724',
+    newtab: false,
+  },
+  {
+    name: 'GitHub',
+    image: 'github-logo.png',
+    value: 'https://github.com/pavel-piatetskii',
+    link: 'https://github.com/pavel-piatetskii',
+    newtab: true,
+  },
+  {
+    name: 'LinkedIn',
+    image: 'linkedin-logo.png',
+    value: 'https://www.linkedin.com/in/pavel-piatetskii-1887241a0/',
+    link: 'https://www.linkedin.com/in/pavel-piatetskii-1887241a0/',
+    newtab: true,
+  },
+];
+
+export default contactsData;
