efadac555eb1459ab1ca8135922726317a11baf2
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-06-10 10:59:05 -0700

    Fill Contacts section

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -5,32 +5,37 @@ export default function Contacts(props) {
   const contactsData = [
     {
       name: 'E-Mail',
-      image: '',
-      value: '',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png',
+      value: 'pavel.piatetskii@gmail.com',
+      link: '#',
     },
     {
       name: 'Phone',
-      image: '',
-      value: '',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Phone_Shiny_Icon.svg/1024px-Phone_Shiny_Icon.svg.png',
+      value: '604-363-17-23',
+      link: '#',
     },
     {
       name: 'GitHub',
-      image: '',
-      value: '',
+      image: 'github-logo.png',
+      value: 'https://github.com/pavel-piatetskii',
+      link: '#',
     },
     {
       name: 'LinkedIn',
-      image: '',
-      value: '',
+      image: 'linkedin-logo.png',
+      value: 'https://www.linkedin.com/in/pavel-piatetskii-1887241a0/',
+      link: '',
     },
   ];
 
   return (<section class="contacts">
     <h2 className="contacts__header">Contacts</h2>
-    {contactsData.map(el => (
+    {contactsData.map(contact => (
       <article className="contacts__container">
-        <span className="contacts__container__name">{el.name}</span>
-        
+        <img className="contacts__container__image" src={contact.image} />
+        <span className="contacts__container__name">{contact.name}:</span>
+        <span className="contacts__container__value">{contact.value}</span>
       </article>
     ))}
   </section>)
