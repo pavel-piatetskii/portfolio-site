35ce94078b110cbb47172b196068c8b483586791
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2024-10-21 22:49:40 -0700

    Update some classes to tailwind, restyle some elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'van-backgr': "url('data/images/background.png')",
+        'doc-download': "url('data/images/doc_download.png')",
+        'pdf-download': "url('data/images/pdf_download.png')",
+      },
+      colors: {
+        'van-cyan': '#285064',
+      },
+    },
   },
   plugins: [],
 }
